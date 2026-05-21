2a0286b295f4c0ddff6956c1034e1a830b0734ab
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-05-21 23:43:45 +0100

    Okay I guess we're using ratatui now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use colored::Colorize;
+use ratatui::{
+    Frame,
+    crossterm::{
+        event::{self, Event, KeyCode},
+        terminal,
+    },
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Line, Span},
+    widgets::Paragraph,
+};
 
 const UNKNOWN: &'static str = "????";
 
@@ -36,7 +46,62 @@ impl DiscoveredName for Room {
     }
 }
 
-fn main() {
+struct Game {
+    current_building: Building,
+    current_room: Room,
+}
+
+fn ui(f: &mut Frame, game: &Game) {
+    let [header_area, middle_area, text_area] = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(1),
+            Constraint::Length(3),
+        ])
+        .areas(f.area());
+
+    let area_name = Span::styled("Loughborough", Color::Blue);
+    let building_name = match game.current_building.name_as_discovered() {
+        Some(name) => Span::styled(name, Color::Blue),
+        None => Span::styled(UNKNOWN, Color::DarkGray),
+    };
+    let room_name = match game.current_room.name_as_discovered() {
+        Some(name) => Span::styled(name, Color::Blue),
+        None => Span::styled(UNKNOWN, Color::DarkGray),
+    };
+
+    let label = |text: &'static str| Span::styled(text, Color::DarkGray);
+    let header = Paragraph::new(vec![
+        Line::from(vec![label("Place: "), area_name]),
+        Line::from(vec![label("Building: "), building_name]),
+        Line::from(vec![label("Room: "), room_name]),
+    ]);
+
+    f.render_widget(header, header_area);
+
+    let room_style = Style::new().add_modifier(Modifier::UNDERLINED);
+    let room_name = match game.current_room.name_as_discovered() {
+        Some(name) => Span::styled(name, room_style.fg(Color::Blue)),
+        None => Span::styled("a room", room_style.fg(Color::DarkGray)),
+    };
+    let room_text = Line::from(vec![
+        Span::styled("> ", Color::DarkGray),
+        Span::raw("You find yourself in "),
+        room_name,
+        Span::raw("."),
+    ]);
+
+    let paragraph = Paragraph::new(room_text);
+
+    f.render_widget(paragraph, text_area);
+}
+
+fn main() -> std::io::Result<()> {
+    terminal::enable_raw_mode()?;
+
+    let mut terminal = ratatui::init();
+
     let wavy_top = Building {
         name: "Wavy Top",
         name_discovered: false,
@@ -46,36 +111,21 @@ fn main() {
         name_discovered: false,
     };
 
-    draw_header(&wavy_top, &room);
-
-    println!();
-    print_entered_room(&room);
-}
-
-fn print_entered_room(room: &Room) {
-    let room_name = match room.name_as_discovered() {
-        Some(name) => name.underline(),
-        None => "a room".bright_black().underline(),
+    let game = Game {
+        current_building: wavy_top,
+        current_room: room,
     };
-    println!("You find yourself in {}.", room_name);
-}
 
-fn draw_header(building: &Building, room: &Room) {
-    println!("{} {}", "Place:".bright_black(), "Loughborough".blue());
-    println!(
-        "{} {}",
-        "Building:".bright_black(),
-        match building.name_as_discovered() {
-            Some(name) => name.blue(),
-            None => UNKNOWN.bright_black(),
+    loop {
+        terminal.draw(|f| ui(f, &game))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                break;
+            }
         }
-    );
-    println!(
-        "{} {}",
-        "Room:".bright_black(),
-        match room.name_as_discovered() {
-            Some(name) => name.blue(),
-            None => UNKNOWN.bright_black(),
-        }
-    );
+    }
+
+    ratatui::restore();
+    Ok(())
 }
