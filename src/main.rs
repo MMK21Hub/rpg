e65fa36363df6ec6f3a2f94d282538d59383a387
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-05-21 23:17:54 +0100

    Get a bulding and a room going

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,81 @@
-use std::fmt::Display;
+use colored::Colorize;
 
-const UNKNOWN_NAME: &'static str = "????";
+const UNKNOWN: &'static str = "????";
+
+trait DiscoveredName {
+    fn name_as_discovered(&self) -> Option<&'static str>;
+}
 
 struct Building {
     name: &'static str,
     name_discovered: bool,
 }
 
-impl Display for Building {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let name = if self.name_discovered {
-            self.name
+impl DiscoveredName for Building {
+    fn name_as_discovered(&self) -> Option<&'static str> {
+        if self.name_discovered {
+            Some(self.name)
         } else {
-            UNKNOWN_NAME
-        };
-        write!(f, "{}", name)
+            None
+        }
+    }
+}
+
+struct Room {
+    name: &'static str,
+    name_discovered: bool,
+}
+
+impl DiscoveredName for Room {
+    fn name_as_discovered(&self) -> Option<&'static str> {
+        if self.name_discovered {
+            Some(self.name)
+        } else {
+            None
+        }
     }
 }
 
 fn main() {
     let wavy_top = Building {
-        name: "Eiffel Tower",
+        name: "Wavy Top",
         name_discovered: false,
     };
-    println!("Building: {}", wavy_top);
+    let room = Room {
+        name: "WAV068",
+        name_discovered: false,
+    };
+
+    draw_header(&wavy_top, &room);
+
+    println!();
+    print_entered_room(&room);
+}
+
+fn print_entered_room(room: &Room) {
+    let room_name = match room.name_as_discovered() {
+        Some(name) => name.underline(),
+        None => "a room".bright_black().underline(),
+    };
+    println!("You find yourself in {}.", room_name);
+}
+
+fn draw_header(building: &Building, room: &Room) {
+    println!("{} {}", "Place:".bright_black(), "Loughborough".blue());
+    println!(
+        "{} {}",
+        "Building:".bright_black(),
+        match building.name_as_discovered() {
+            Some(name) => name.blue(),
+            None => UNKNOWN.bright_black(),
+        }
+    );
+    println!(
+        "{} {}",
+        "Room:".bright_black(),
+        match room.name_as_discovered() {
+            Some(name) => name.blue(),
+            None => UNKNOWN.bright_black(),
+        }
+    );
 }
