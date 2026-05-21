6c9c1009d635a05e080b31599217d5714391c932
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-05-22 00:31:16 +0100

    Registries and IDs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use ratatui::{
     Frame,
     crossterm::{
@@ -16,6 +18,8 @@ trait DiscoveredName {
     fn name_as_discovered(&self) -> Option<&'static str>;
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct BuildingId(&'static str);
 struct Building {
     name: &'static str,
     name_discovered: bool,
@@ -31,6 +35,8 @@ impl DiscoveredName for Building {
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct RoomId(&'static str);
 struct Room {
     name: &'static str,
     name_discovered: bool,
@@ -46,27 +52,66 @@ impl DiscoveredName for Room {
     }
 }
 
+enum Message {
+    EnteredRoom(RoomId),
+}
+
+struct World {
+    buildings: HashMap<BuildingId, Building>,
+    rooms: HashMap<RoomId, Room>,
+}
+
+impl World {
+    fn building(&self, id: BuildingId) -> &Building {
+        self.buildings
+            .get(&id)
+            .unwrap_or_else(|| panic!("Missing BuildingId: {:?}", id))
+    }
+
+    fn room(&self, id: RoomId) -> &Room {
+        self.rooms
+            .get(&id)
+            .unwrap_or_else(|| panic!("Missing RoomId: {:?}", id))
+    }
+}
+
 struct Game {
-    current_building: Building,
-    current_room: Room,
+    current_building: BuildingId,
+    current_room: RoomId,
+    messages: Vec<Message>,
+    world: World,
+}
+
+impl Game {
+    fn enter_room(&mut self, room: RoomId) {
+        self.current_room = room;
+        self.messages.push(Message::EnteredRoom(room));
+    }
+    fn current_building(&self) -> &Building {
+        self.world.building(self.current_building)
+    }
+    fn current_room(&self) -> &Room {
+        self.world.room(self.current_room)
+    }
 }
 
 fn ui(f: &mut Frame, game: &Game) {
-    let [header_area, middle_area, text_area] = Layout::default()
+    let message_area_height: u16 = game.messages.len().try_into().unwrap_or(u16::MAX);
+    let [header_area, middle_area, message_area] = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
             Constraint::Length(3),
             Constraint::Min(1),
-            Constraint::Length(3),
+            Constraint::Length(message_area_height),
         ])
         .areas(f.area());
 
     let area_name = Span::styled("Loughborough", Color::Blue);
-    let building_name = match game.current_building.name_as_discovered() {
+    let building_name = match game.current_building().name_as_discovered() {
         Some(name) => Span::styled(name, Color::Blue),
         None => Span::styled(UNKNOWN, Color::DarkGray),
     };
-    let room_name = match game.current_room.name_as_discovered() {
+    let room_name = match game.current_room().name_as_discovered() {
         Some(name) => Span::styled(name, Color::Blue),
         None => Span::styled(UNKNOWN, Color::DarkGray),
     };
@@ -81,7 +126,7 @@ fn ui(f: &mut Frame, game: &Game) {
     f.render_widget(header, header_area);
 
     let room_style = Style::new().add_modifier(Modifier::UNDERLINED);
-    let room_name = match game.current_room.name_as_discovered() {
+    let room_name = match game.current_room().name_as_discovered() {
         Some(name) => Span::styled(name, room_style.fg(Color::Blue)),
         None => Span::styled("a room", room_style.fg(Color::DarkGray)),
     };
@@ -94,7 +139,7 @@ fn ui(f: &mut Frame, game: &Game) {
 
     let paragraph = Paragraph::new(room_text);
 
-    f.render_widget(paragraph, text_area);
+    f.render_widget(paragraph, message_area);
 }
 
 fn main() -> std::io::Result<()> {
@@ -102,19 +147,29 @@ fn main() -> std::io::Result<()> {
 
     let mut terminal = ratatui::init();
 
-    let wavy_top = Building {
-        name: "Wavy Top",
-        name_discovered: false,
-    };
-    let room = Room {
-        name: "WAV068",
-        name_discovered: false,
+    let mut game = Game {
+        current_building: BuildingId("wavy_top"),
+        current_room: RoomId("wav_068"),
+        messages: vec![],
+        world: World {
+            buildings: HashMap::from([(
+                BuildingId("wavy_top"),
+                Building {
+                    name: "Wavy Top",
+                    name_discovered: false,
+                },
+            )]),
+            rooms: HashMap::from([(
+                RoomId("wav_068"),
+                Room {
+                    name: "WAV068",
+                    name_discovered: false,
+                },
+            )]),
+        },
     };
 
-    let game = Game {
-        current_building: wavy_top,
-        current_room: room,
-    };
+    game.messages.push(Message::EnteredRoom(game.current_room));
 
     loop {
         terminal.draw(|f| ui(f, &game))?;
